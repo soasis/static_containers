e2f3e9e35697d5e691a5d4588454bc2f6df89a27
ThePhD <phdofthehouse@gmail.com>
2021-02-24 00:02:17 -0500

    🤷‍♀️ Messing around
    - Might make this whole thing constexpr later, just for fun

---
+++ include/ztd/fixed_vector/detail/uninit.hpp
@@ -0,0 +1,61 @@
+//   -----------------
+// | ztd::fixed_vector |
+//   -----------------
+
+// Written 2019 - 2021 by ThePhD <phdofthehouse@gmail.com>
+
+// To the extent possible under law, the author(s) have dedicated all copyright and related
+// and neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+#pragma once
+
+#ifndef ZTD_FIXED_VECTOR_DETAIL_UNINIT_HPP
+#define ZTD_FIXED_VECTOR_DETAIL_UNINIT_HPP
+
+#include <ztd/fixed_vector/version.hpp>
+
+#include <utility>
+
+namespace ztd {
+
+	ZTD_FIXED_VECTOR_INLINE_ABI_NAMESPACE_OPEN_I_
+
+	namespace __fv_detail {
+		template <typename _Type>
+		struct alignas(_Type) __uninit {
+			constexpr __uninit() : _M_dummy() {
+			}
+
+			template <typename... _Args>
+			constexpr __uninit(::std::in_place_t, _Args&&... __args) : _M_value(::std::forward<_Args>(__args)...) {
+			}
+
+			friend _Type& __unwrap_value(__uninit& __wrapped_value) noexcept {
+				return __wrapped_value._M_value;
+			}
+
+			friend const _Type& __unwrap_value(const __uninit& __wrapped_value) noexcept {
+				return __wrapped_value._M_value;
+			}
+
+			friend _Type&& __unwrap_value(__uninit&& __wrapped_value) noexcept {
+				return ::std::move(__wrapped_value._M_value);
+			}
+
+			union {
+				char _M_dummy;
+				_Type _M_value;
+			};
+		};
+
+	} // namespace __fv_detail
+
+	ZTD_FIXED_VECTOR_INLINE_ABI_NAMESPACE_CLOSE_I_
+
+} // namespace ztd
+
+#endif // ZTD_FIXED_VECTOR_DETAIL_UNINIT_HPP
--- include/ztd/fixed_vector/fixed_vector.hpp
+++ include/ztd/fixed_vector/fixed_vector.hpp
@@ -1,8 +1,8 @@
 //   -----------------
-// | phd::fixed_vector |
+// | ztd::fixed_vector |
 //   -----------------
 
-// Written in 2019 by ThePhD <phdofthehouse@gmail.com>
+// Written 2019 - 2021 by ThePhD <phdofthehouse@gmail.com>
 
 // To the extent possible under law, the author(s) have dedicated all copyright and related
 // and neighboring rights to this software to the public domain worldwide. This software is
@@ -13,8 +13,8 @@
 
 #pragma once
 
-#ifndef PHD_FIXED_VECTOR_FIXED_VECTOR_HPP
-#define PHD_FIXED_VECTOR_FIXED_VECTOR_HPP
+#ifndef ZTD_FIXED_VECTOR_FIXED_VECTOR_HPP
+#define ZTD_FIXED_VECTOR_FIXED_VECTOR_HPP
 
 #include <phd/fixed_vector/detail/uninit.hpp>
 #include <phd/fixed_vector/detail/wrap_pointer.hpp>
@@ -25,7 +25,8 @@
 #include <memory>
 #include <cassert>
 
-namespace phd { inline namespace __0 {
+namespace ztd {
+	ZTD_FIXED_VECTOR_INLINE_ABI_NAMESPACE_OPEN_I_
 
 	template <typename _Ty, ::std::size_t _Capacity>
 	class fixed_vector {
@@ -35,8 +36,8 @@ namespace phd { inline namespace __0 {
 		using const_reference = ::std::add_lvalue_reference_t<const _Ty>;
 		using pointer         = ::std::add_pointer_t<_Ty>;
 		using const_pointer   = ::std::add_pointer_t<const _Ty>;
-		using iterator        = __detail::__wrap_pointer<__detail::__uninit<_Ty>>;
-		using const_iterator  = __detail::__wrap_pointer<const __detail::__uninit<_Ty>>;
+		using iterator        = __fv_detail::__wrap_pointer<__fv_detail::__uninit<_Ty>>;
+		using const_iterator  = __fv_detail::__wrap_pointer<const __fv_detail::__uninit<_Ty>>;
 		using size_type       = ::std::size_t;
 		using difference_type = ::std::ptrdiff_t;
 		inline static constexpr const ::std::size_t inline_capacity = _Capacity;
@@ -54,14 +55,14 @@ namespace phd { inline namespace __0 {
 		}
 
 		fixed_vector(size_type __count) noexcept : _M_size(), _M_elements() {
-			this->insert(this->cend(), __count, value_type{});
+			this->insert(this->cend(), __count, value_type {});
 		}
 
 		template <typename... _Args>
 		void emplace_back(_Args&&... __args) noexcept(::std::is_nothrow_constructible_v<value_type, _Args...>) {
 			__alloc __al;
 			__alloc_traits::construct(
-				__al, ::std::addressof(this->_M_elements[this->_M_size]), ::std::forward<_Args>(__args)...);
+			     __al, ::std::addressof(this->_M_elements[this->_M_size]), ::std::forward<_Args>(__args)...);
 			++this->_M_size;
 		}
 
@@ -74,7 +75,7 @@ namespace phd { inline namespace __0 {
 		void push_back(value_type&& __value) noexcept(::std::is_nothrow_move_constructible_v<value_type>) {
 			__alloc __al;
 			__alloc_traits::construct(
-				__al, ::std::addressof(this->_M_elements[this->_M_size]), ::std::move(__value));
+			     __al, ::std::addressof(this->_M_elements[this->_M_size]), ::std::move(__value));
 			++this->_M_size;
 		}
 
@@ -93,8 +94,8 @@ namespace phd { inline namespace __0 {
 
 		template <typename... _Args>
 		iterator emplace(const_iterator __where, _Args&&... __args) noexcept(
-			::std::is_nothrow_constructible_v<value_type,
-			     _Args...> && (::std::is_nothrow_move_assignable_v<value_type> || ::std::is_nothrow_copy_assignable_v<value_type>)) {
+		     ::std::is_nothrow_constructible_v<value_type,
+		          _Args...> && (::std::is_nothrow_move_assignable_v<value_type> || ::std::is_nothrow_copy_assignable_v<value_type>)) {
 			iterator __where_last = this->cend();
 			if (this->empty() || __where == __where_last) {
 				this->emplace_back(::std::forward<_Args>(__args)...);
@@ -107,7 +108,8 @@ namespace phd { inline namespace __0 {
 			iterator __where_p      = __where_first - 2;
 			iterator __from_where_p = __where_last - 1;
 
-			__alloc_traits::construct(__al, to_address(__where_last), ::std::move(*__from_where_p));
+			__alloc_traits::construct(
+			     __al, __fv_detail::__adl::__adl_to_address(__where_last), ::std::move(*__from_where_p));
 
 			for (; __from_where_p != __where_first; --__where_p, (void)__from_where_p) {
 				if constexpr (::std::is_nothrow_move_assignable_v<value_type>) {
@@ -117,29 +119,30 @@ namespace phd { inline namespace __0 {
 					*__where_p = *__from_where_p;
 				}
 			}
-			__alloc_traits::construct(__al, to_address(__where_first), ::std::forward<_Args>(__args)...);
+			__alloc_traits::construct(
+			     __al, __fv_detail::__adl::__adl_to_address(__where_first), ::std::forward<_Args>(__args)...);
 		}
 
 		iterator insert(const_iterator __where, value_type&& __value) noexcept(
-			::std::is_nothrow_move_constructible_v<
-			     value_type> && (::std::is_nothrow_move_assignable_v<value_type> || ::std::is_nothrow_copy_assignable_v<value_type>)) {
+		     ::std::is_nothrow_move_constructible_v<
+		          value_type> && (::std::is_nothrow_move_assignable_v<value_type> || ::std::is_nothrow_copy_assignable_v<value_type>)) {
 			return this->emplace(::std::move(__where), ::std::move(__value));
 		}
 
 		iterator insert(const_iterator __where, const value_type& __value) noexcept(
-			::std::is_nothrow_copy_constructible_v<
-			     value_type> && (::std::is_nothrow_move_assignable_v<value_type> || ::std::is_nothrow_copy_assignable_v<value_type>)) {
+		     ::std::is_nothrow_copy_constructible_v<
+		          value_type> && (::std::is_nothrow_move_assignable_v<value_type> || ::std::is_nothrow_copy_assignable_v<value_type>)) {
 			return this->emplace(::std::move(__where), __value);
 		}
 
 		iterator insert(const_iterator __where, ::std::initializer_list<value_type> __values) noexcept(
-			::std::is_nothrow_copy_constructible_v<
-			     value_type> && (::std::is_nothrow_move_assignable_v<value_type> || ::std::is_nothrow_copy_assignable_v<value_type>)) {
+		     ::std::is_nothrow_copy_constructible_v<
+		          value_type> && (::std::is_nothrow_move_assignable_v<value_type> || ::std::is_nothrow_copy_assignable_v<value_type>)) {
 			return this->insert(::std::move(__where), __values.begin(), __values.end());
 		}
 
 		iterator insert(const_iterator __where, size_type __count) noexcept {
-			return this->insert(::std::move(__where), __count, value_type{});
+			return this->insert(::std::move(__where), __count, value_type {});
 		}
 
 		iterator insert(const_iterator __where, size_type __count, const value_type& __value) noexcept {
@@ -153,9 +156,9 @@ namespace phd { inline namespace __0 {
 		}
 
 		template <typename _First, typename _Last,
-			::std::enable_if_t<
-			     !::std::is_integral_v<
-			          _First> && !::std::is_same_v<::std::remove_cvref_t<_Last>, value_type>>* = nullptr>
+		     ::std::enable_if_t<
+		          !::std::is_integral_v<
+		               _First> && !::std::is_same_v<::std::remove_cvref_t<_Last>, value_type>>* = nullptr>
 		iterator insert(const_iterator __where, _First __first, _Last __last) {
 			if (__first == __last) {
 				return iterator(const_cast<pointer>(__where.base()));
@@ -168,7 +171,7 @@ namespace phd { inline namespace __0 {
 				auto __fl_dist = __last - __first;
 				assert(static_cast<size_type>(__fl_dist + __where_dist) <= this->capacity());
 				return this->_M_unchecked_multi_insert_random_access(
-					::std::move(__where), __where_dist, ::std::move(__first), ::std::move(__last));
+				     ::std::move(__where), __where_dist, ::std::move(__first), ::std::move(__last));
 			}
 			else {
 				iterator __where_first     = this->emplace(::std::move(__where), *__first);
@@ -205,7 +208,7 @@ namespace phd { inline namespace __0 {
 				return this->begin();
 			}
 			return this->_M_unchecked_multi_erase(
-				::std::move(__where_first), ::std::move(__where_last), __where_diff);
+			     ::std::move(__where_first), ::std::move(__where_last), __where_diff);
 		}
 
 		constexpr void pop_back() noexcept {
@@ -291,7 +294,7 @@ namespace phd { inline namespace __0 {
 
 			if (this->size() < __count) {
 				size_type __diff = __count - this->size();
-				this->insert(this->cend(), __diff, value_type{});
+				this->insert(this->cend(), __diff, value_type {});
 			}
 			else if (this->size() > __count) {
 				size_type __diff = this->size() - __count;
@@ -321,7 +324,7 @@ namespace phd { inline namespace __0 {
 
 	private:
 		size_type _M_size;
-		__detail::__uninit<value_type> _M_elements[_Capacity];
+		__fv_detail::__uninit<value_type> _M_elements[_Capacity];
 
 		constexpr pointer _M_data_end() {
 			return ::std::addressof(this->_M_elements[this->_M_size]._M_value);
@@ -335,7 +338,7 @@ namespace phd { inline namespace __0 {
 			iterator __where_first(const_cast<pointer>(__where_cp));
 			iterator __where_last = this->begin() + this->size();
 			for (iterator __where_p = __where_first, __from_where_p = __where_first + 1;
-				__from_where_p != __where_last; ++__where_p, (void)++__from_where_p) {
+			     __from_where_p != __where_last; ++__where_p, (void)++__from_where_p) {
 				if constexpr (::std::is_nothrow_move_assignable_v<value_type>) {
 					*__where_p = ::std::move(*__from_where_p);
 				}
@@ -344,9 +347,9 @@ namespace phd { inline namespace __0 {
 				}
 				else {
 					static_assert(::std::is_nothrow_copy_assignable_v<value_type>,
-						"this type is neither nothrow copy-assignable or nothrow move-constructible, making "
-						"it "
-						"impossible to properly copy in a constexpr constext in a pre-C++20 world.");
+					     "this type is neither nothrow copy-assignable or nothrow move-constructible, making "
+					     "it "
+					     "impossible to properly copy in a constexpr constext in a pre-C++20 world.");
 				}
 			}
 			__where_last->~value_type();
@@ -355,19 +358,19 @@ namespace phd { inline namespace __0 {
 		}
 
 		constexpr iterator _M_unchecked_multi_erase(
-			const_iterator __first, const_iterator __last, difference_type __where_diff) noexcept {
+		     const_iterator __first, const_iterator __last, difference_type __where_diff) noexcept {
 			iterator __where_first(const_cast<pointer>(__first.base()));
 			iterator __where_last      = this->end();
 			iterator __where_diff_last = __where_last - __where_diff;
 			if constexpr (::std::is_nothrow_move_assignable_v<value_type>) {
 				for (iterator __where_p = __where_first, __from_where_p = __where_diff_last;
-					__from_where_p != __where_first; ++__where_p, (void)++__from_where_p) {
+				     __from_where_p != __where_first; ++__where_p, (void)++__from_where_p) {
 					*__where_p = ::std::move(*__from_where_p);
 				}
 			}
 			else if (::std::is_nothrow_copy_assignable_v<value_type>) {
 				for (iterator __where_p = __where_first, __from_where_p = __where_diff_last;
-					__from_where_p != __where_first; ++__where_p, (void)++__from_where_p) {
+				     __from_where_p != __where_first; ++__where_p, (void)++__from_where_p) {
 					*__where_p = *__from_where_p;
 				}
 			}
@@ -381,8 +384,8 @@ namespace phd { inline namespace __0 {
 #endif
 			else {
 				static_assert(::std::is_nothrow_copy_assignable_v<value_type>,
-					"this type is neither nothrow copy-assignable or nothrow move-constructible, making it "
-					"impossible to properly copy in a constexpr constext in a pre-C++20 world.");
+				     "this type is neither nothrow copy-assignable or nothrow move-constructible, making it "
+				     "impossible to properly copy in a constexpr constext in a pre-C++20 world.");
 			}
 			for (pointer __where_p = __where_last - 1;; --__where_p) {
 				__where_p->~value_type();
@@ -410,6 +413,8 @@ namespace phd { inline namespace __0 {
 		}
 	};
 
-}} // namespace phd::__0
+	ZTD_FIXED_VECTOR_INLINE_ABI_NAMESPACE_CLOSE_I_
 
-#endif // PHD_FIXED_VECTOR_FIXED_VECTOR_HPP
+} // namespace ztd
+
+#endif // ZTD_FIXED_VECTOR_FIXED_VECTOR_HPP
